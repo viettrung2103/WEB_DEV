fd0fea23c8560d092c95934f9ad1bac011211637
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-11-17 23:25:29 +0200

    fix e2.4

--- Javascipts/exercise/module2/e4/script.js
+++ Javascipts/exercise/module2/e4/script.js
@@ -1,8 +1,14 @@
 // number = +prompt("Enter number");
-let numberList = [];
-do {
-  number = +prompt("Enter number");
-  numberList.push(number);
-} while (number !== 0);
-numberList.sort((a, b) => b > a);
+"strict mode";
+
+const createNumList = () => {
+  let numberList = [];
+  do {
+    number = +prompt("Enter number");
+    numberList.push(number);
+  } while (number !== 0);
+  return numberList;
+};
+let numberList = createNumList();
+numberList.sort((a, b) => b - a);
 numberList.forEach((number) => console.log(number));
