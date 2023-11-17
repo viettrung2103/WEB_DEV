30fcaf018ca30e827b39ce4f023a751b8ff8d613
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-11-17 21:42:08 +0200

    fix e2.1

--- Javascipts/exercise/module2/e1/script.js
+++ Javascipts/exercise/module2/e1/script.js
@@ -8,39 +8,47 @@ const getNumberList = () => {
   }
   return numList;
 };
-let numberList = [];
-console.log(numberList.length);
-const getReverseList = (numberList) => {
-  let reversedList = [];
-  if (numberList != []) {
-    // let biggest = numberList[0];
-    while (numberList.length > 0) {
-      // console.log(`number List: ${numberList}`);
-      let biggest = numberList[0];
-      let indexToRemove = 0;
-      for (let i = 0; i < numList.length; i++) {
-        if (numList[i] < biggest) {
-          // do nothing
-        } else {
-          biggest = numList[i];
-          indexToRemove = i;
-        }
-
-        //find the biggest number in the list
-        // remove the number  from the old list  by finding its index
-        // add the newly found biggest number to new list
-      }
-      numList.splice(indexToRemove, 1);
-      reversedList.push(biggest);
-      // console.log(`reversed list: ${reversedList}`);
-    }
+// let numberList = [];
+const printInReverseOrder = (numberList) => {
+  // console.log(numberList.length);
+  for (let i = numberList.length - 1; i >= 0; i--) {
+    // console.log(i);
+    console.log(numberList[i]);
   }
-  return reversedList;
+  return;
 };
+// const getReverseList = (numberList) => {
+//   let reversedList = [];
+//   if (numberList != []) {
+//     // let biggest = numberList[0];
+//     while (numberList.length > 0) {
+//       // console.log(`number List: ${numberList}`);
+//       let biggest = numberList[0];
+//       let indexToRemove = 0;
+//       for (let i = 0; i < numList.length; i++) {
+//         if (numList[i] < biggest) {
+//           // do nothing
+//         } else {
+//           biggest = numList[i];
+//           indexToRemove = i;
+//         }
+
+//         //find the biggest number in the list
+//         // remove the number  from the old list  by finding its index
+//         // add the newly found biggest number to new list
+//       }
+//       numList.splice(indexToRemove, 1);
+//       reversedList.push(biggest);
+//       // console.log(`reversed list: ${reversedList}`);
+//     }
+//   }
+//   return reversedList;
+// };
 
 const numList = getNumberList();
 // console.log(numList);
-
-const reversedList = getReverseList(numList);
 // console.log(numList);
-reversedList.forEach((currentNumber) => console.log(currentNumber));
+printInReverseOrder(numList);
+// const reversedList = getReverseList(numList);
+// // console.log(numList);
+// reversedList.forEach((currentNumber) => console.log(currentNumber));
