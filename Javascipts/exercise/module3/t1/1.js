6489e00eb4dbde1999becd7bbf59b82a0f818e7b
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-11-29 14:40:14 +0200

    finish e3.3 and fix 3.1

--- Javascipts/exercise/module3/t1/1.js
+++ Javascipts/exercise/module3/t1/1.js
@@ -1,17 +1,8 @@
-const textArray = ["First item", "Second item", "Third item"];
-
 const ul = document.querySelector("#target");
-
-const createListItem = (stringItem) => {
-  const listItem = document.createElement("li");
-  listItem.classList = "list-item";
-  listItem.textContent = stringItem;
-  ul.appendChild(listItem);
-};
-
-const listItemDisplay = () => {
-  textArray.forEach((currentString) => createListItem(currentString));
-};
-
-listItemDisplay();
 ul.classList = "my-list";
+const content = `
+<li>First item</li>
+<li>Second item</li>
+<li>Third item</li>
+`;
+ul.innerHTML = content;
--- Javascipts/exercise/module3/t3/3.js
+++ Javascipts/exercise/module3/t3/3.js
@@ -1,2 +1,12 @@
-'use strict';
-const names = ['John', 'Paul', 'Jones'];
+"use strict";
+const names = ["John", "Paul", "Jones"];
+
+const element = document.querySelector("#target");
+
+// console.log(names);
+for (const user of names) {
+  // console.log(user);
+  const newLi = document.createElement("li");
+  newLi.textContent = user;
+  element.appendChild(newLi);
+}
