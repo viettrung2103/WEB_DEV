55cdfc8f24d5c2c1eba0f1e00b8b377d549d4a41
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2023-11-29 15:34:33 +0200

    complete e3.5, create multi articles

--- Javascipts/exercise/module3/t4/4.js
+++ Javascipts/exercise/module3/t4/4.js
@@ -1,15 +1,26 @@
-'use strict';
+"use strict";
 const students = [
   {
-    name: 'John',
-    id: '2345768',
+    name: "John",
+    id: "2345768",
   },
   {
-    name: 'Paul',
-    id: '2134657',
+    name: "Paul",
+    id: "2134657",
   },
   {
-    name: 'Jones',
-    id: '5423679',
+    name: "Jones",
+    id: "5423679",
   },
 ];
+
+const target = document.querySelector("#target");
+
+const addItemToTarget = (obj) => {
+  const option = document.createElement("option");
+  option.value = `${obj.id}`;
+  option.textContent = `${obj.name}`;
+  target.appendChild(option);
+};
+
+students.forEach((student) => addItemToTarget(student));
